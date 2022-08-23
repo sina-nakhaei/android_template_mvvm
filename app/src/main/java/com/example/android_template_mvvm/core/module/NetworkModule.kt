7f9bb78b5b96c1501183ee2c962bdbed1e3593e9
Sina <36819672+cnankh@users.noreply.github.com>
2022-08-23 11:28:12 +0430

    setup NetworkModule

---
+++ app/src/main/java/com/example/android_template_mvvm/core/module/NetworkModule.kt
@@ -0,0 +1,49 @@
+package com.example.android_template_mvvm.core.module
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+    @Provides
+    @Singleton
+    fun provideBaseUrl() = ""
+
+    @Provides
+    @Singleton
+    fun provideKtorClient(): HttpClient {
+        return HttpClient(OkHttp) {
+            expectSuccess = true
+            defaultRequest {
+                host = provideBaseUrl()
+                url {
+                    protocol = URLProtocol.HTTPS
+                }
+            }
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.HEADERS
+            }
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+}
