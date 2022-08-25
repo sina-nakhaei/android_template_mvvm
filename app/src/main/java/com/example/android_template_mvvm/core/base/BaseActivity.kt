84e16fb9cbcd54ce83b1f0b0a587826d40cecd8e
Sina <36819672+cnankh@users.noreply.github.com>
2022-08-25 10:15:36 +0430

    feat: <BaseActivity> create class

---
+++ app/src/main/java/com/example/android_template_mvvm/core/base/BaseActivity.kt
@@ -0,0 +1,57 @@
+package com.example.android_template_mvvm.core.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import javax.inject.Inject
+
+abstract class BaseActivity<VBinding : ViewBinding> :
+    AppCompatActivity() {
+
+
+
+    private var _binding: VBinding? = null
+    protected val binding get() = _binding!!
+
+    abstract fun getViewBinding(): VBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+        setContentView(binding.root)
+
+        primary()
+        configuration()
+        viewsConfiguration()
+        observer()
+    }
+
+    /**
+     * anything needs to be run as soon as view is created goes here
+     */
+    open fun primary() {}
+
+    /**
+     * here is the place to setup your primary configuration
+     */
+    open fun configuration() {}
+
+    /**
+     * anything you need to do with views implemented here
+     */
+    open fun viewsConfiguration() {}
+
+    /**
+     * observe your data here
+     */
+    open fun observer() {}
+
+    private fun init() {
+        _binding = getViewBinding()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
