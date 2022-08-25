c9ce2f4f483917ceb7eac3a8e0c31f47e0d65282
Sina <36819672+cnankh@users.noreply.github.com>
2022-08-25 08:42:21 +0430

    feat: <BaseViewModel> create class

--- app/src/main/java/com/example/android_template_mvvm/core/base/BaseViewModel.kt
+++ app/src/main/java/com/example/android_template_mvvm/core/base/BaseViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.android_template_mvvm.core.base
 
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-abstract class BaseViewModel : ViewModel() {
+abstract class BaseViewModel(private val repository: BaseRepository) :
+    ViewModel() {
 }
