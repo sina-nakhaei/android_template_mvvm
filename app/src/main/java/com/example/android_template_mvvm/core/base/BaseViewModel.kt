ef65e1a0a59401976d38b6067ba64bd4979dda0e
Sina <36819672+cnankh@users.noreply.github.com>
2022-08-26 17:43:30 +0430

    update BaseViewModel

--- app/src/main/java/com/example/android_template_mvvm/core/base/BaseViewModel.kt
+++ app/src/main/java/com/example/android_template_mvvm/core/base/BaseViewModel.kt
@@ -1,8 +1,25 @@
 package com.example.android_template_mvvm.core.base
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
-abstract class BaseViewModel(private val repository: BaseRepository) :
-    ViewModel() {
+abstract class BaseViewModel(
+    private val repository: BaseRepository,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) :
+    ViewModel(), CoroutineScope {
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = job + dispatcher
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+
 }
